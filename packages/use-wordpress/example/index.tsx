f0b4bf4965e6547b3850a21ca80a382b997ffd64
Josh Pollock <josh@saturdaydrive.io>
2019-10-30 15:48:35 -0400

    pagination in example

--- packages/use-wordpress/example/index.tsx
+++ packages/use-wordpress/example/index.tsx
@@ -6,36 +6,43 @@ import { useRef } from 'react';
 export default function useWordPress(endpoint: string) {
   let wp = useRef(new WPAPI({ endpoint }));
 
-  return {
-    wp: wp.current,
-  };
+  return [wp.current];
 }
 
 const App = () => {
   const [page, setPage] = React.useState(1);
   const [posts, setPosts] = React.useState<Array<any>>([]);
-  const handlePageChange = () => {
+  const [wp] = useWordPress('https://calderaforms.com/wp-json');
+  const nextPage = () => {
     setPage(page + 1);
+  };
+  const previousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
   };
   const [paging, setPaging] = React.useState({
     total: 0,
     totalPages: 0,
   });
-  let wp = useRef(new WPAPI({ endpoint: 'https://calderaforms.com/wp-json' }));
 
   React.useEffect(() => {
+    if (!wp) {
+      return;
+    }
     let isSubscribed = true;
-    wp.current
-      .posts()
+    wp.posts()
       .page(page)
       .then(data => {
+        console.log(data);
         if (isSubscribed) {
           const _posts: Array<any> = [];
           Object.keys(data).forEach((key: string) => {
-            if ('__paging' !== key && data[key].hasOwnProperty('id')) {
+            if ('_paging' !== key && data[key].hasOwnProperty('id')) {
               _posts.push(data[key]);
             }
           });
+          setPaging(data._paging);
           setPosts(_posts);
         }
       })
@@ -50,7 +57,20 @@ const App = () => {
   return (
     <div>
       <div>
-        <button onClick={handlePageChange}>Next Page</button>
+        <button onClick={previousPage} disabled={1 === page ? true : false}>
+          Previous Page
+        </button>
+        <button
+          onClick={nextPage}
+          disabled={paging.totalPages === page ? true : false}
+        >
+          Next Page
+        </button>
+        <ul>
+          <li>Total Pages: {paging.totalPages}</li>
+          <li>Current Page: {page}</li>
+          <li>Total Posts: {paging.total}</li>
+        </ul>
       </div>
       {posts.length && (
         <ul>
@@ -59,7 +79,7 @@ const App = () => {
             return <li key={post.id}>{post.title.rendered}</li>;
           })}
         </ul>
-      )}{' '}
+      )}
     </div>
   );
 };
